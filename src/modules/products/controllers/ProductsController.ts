c3c38509e85227a15be08a9ed34706bcc5c59e57
Gustavo Souza <gustavo.souzagu@gmail.com>
2021-06-28 19:30:30 -0300

    Criaçao dos Recursos do Modulo de Produtos

---
+++ src/modules/products/controllers/ProductsController.ts
@@ -0,0 +1,67 @@
+import { Request, Response } from 'express';
+import CreateProductService from '../services/CreateProductService';
+import DeleteProductService from '../services/DeleteProductService';
+import ListProductService from '../services/ListProductService';
+import ShowProductService from '../services/ShowProductService';
+import UpdateProductService from '../services/UpdateProductService';
+
+export default class ProductsController {
+  //Controller para Listar todos os Produtos
+  public async index(request: Request, response: Response): Promise<Response> {
+    const listProducts = new ListProductService();
+
+    const product = await listProducts.execute();
+
+    return response.json(product);
+  }
+  //Controller para listar um produto específico
+  public async show(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+
+    const showProduct = new ShowProductService();
+
+    const product = await showProduct.execute({ id });
+
+    return response.json(product);
+  }
+
+  //controller para criar um produto
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { name, price, quantity } = request.body;
+
+    const createProduct = new CreateProductService();
+
+    const product = await createProduct.execute({
+      name,
+      price,
+      quantity,
+    });
+
+    return response.json(product);
+  }
+
+  //Controller para atualizar alguma informação em um produto existente
+  public async update(request: Request, response: Response): Promise<Response> {
+    const { name, price, quantity } = request.body;
+    const { id } = request.params;
+
+    const updateProduct = new UpdateProductService();
+    const product = await updateProduct.execute({
+      id,
+      name,
+      price,
+      quantity,
+    });
+    return response.json(product);
+  }
+  //Controller para deletar produtos
+  public async delete(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+
+    const deleteProduct = new DeleteProductService();
+
+    await deleteProduct.execute({ id });
+
+    return response.json([]);
+  }
+}
---
+++ src/modules/products/services/UpdateProductService.ts
@@ -0,0 +1,42 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import Product from '../typeorm/entities/Product';
+import { ProductRepository } from '../typeorm/repositories/ProductsRepository';
+
+interface IRequest {
+  id: string;
+  name: string;
+  price: number;
+  quantity: number;
+}
+
+class UpdateProductService {
+  public async execute({
+    id,
+    name,
+    price,
+    quantity,
+  }: IRequest): Promise<Product> {
+    const productsRepository = getCustomRepository(ProductRepository);
+
+    const product = await productsRepository.findOne(id);
+    //Validação se o Código do produto existe.
+    if (!product) {
+      throw new AppError('Produto Não Localizado!');
+    }
+    const productExists = await productsRepository.findByName(name);
+    //Valida se não existe nenhum produto com o mesmo nome.
+    if (productExists) {
+      throw new AppError('Produto já cadastrado');
+    }
+
+    product.name = name;
+    product.price = price;
+    product.quantity = quantity;
+
+    await productsRepository.save(product);
+
+    return product;
+  }
+}
+export default UpdateProductService;
